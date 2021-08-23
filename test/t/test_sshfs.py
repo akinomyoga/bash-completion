4971ed3acf796b9a69acf047efd7ca1bfb5f1a3d
Koichi Murase <myoga.murase@gmail.com>
2021-08-23 16:02:23 +0900

    debug(tree): output words and IFS

--- test/t/test_sshfs.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.bashcomp(ignore_env=r"^[+-]_scp_path_esc=")
-class TestSshfs:
-    @pytest.mark.complete("sshfs ./")
-    def test_1(self, completion):
-        assert completion
