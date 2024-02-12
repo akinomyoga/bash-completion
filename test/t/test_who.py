c1c2b8929b4a58f419cd3b9e7293992fb886d8a4
Koichi Murase <myoga.murase@gmail.com>
2024-02-12 20:56:45 +0900

    test(who): update xfail

--- test/t/test_who.py
+++ test/t/test_who.py
@@ -1,11 +1,9 @@
 import pytest
-from conftest import assert_bash_exec
 
 
 class TestWho:
     @pytest.mark.complete(
-        "who --", require_cmd=True, xfail="! who --help &>/dev/null"
+        "who --", require_cmd=True, xfail="! who --help 2>&1 | grep -qe --"
     )
     def test_1(self, bash, completion):
-        assert_bash_exec(bash, "who --help")
         assert completion
