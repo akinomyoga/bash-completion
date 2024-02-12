da8a50afcda4375f775a035c673a4e235270feea
Koichi Murase <myoga.murase@gmail.com>
2024-02-12 20:07:08 +0900

    fix: print "who --help"

--- test/t/test_who.py
+++ test/t/test_who.py
@@ -1,9 +1,11 @@
 import pytest
+from conftest import assert_bash_exec
 
 
 class TestWho:
     @pytest.mark.complete(
         "who --", require_cmd=True, xfail="! who --help &>/dev/null"
     )
-    def test_1(self, completion):
+    def test_1(self, bash, completion):
+        assert_bash_exec(bash, "who --help")
         assert completion
